d5ec8d5256125e5be37ea76c64e6f3b42022c061
Wayne <3522373084@qq.com>
2022-01-16 21:29:20 +0800

    setnx 与 expire 通过一个命令来执行，防止在获取锁和 设置过期时间两个操作之间服务器发生错误导致锁被占用,无法及时得到释放，造成死锁

--- distributed_lock/src/main/java/com/study/distributed_lock/LockTest.java
+++ distributed_lock/src/main/java/com/study/distributed_lock/LockTest.java
@@ -1,5 +1,7 @@
 package com.study.distributed_lock;
 
+import redis.clients.jedis.params.SetParams;
+
 /**
  * @author yang
  * @date 2022/01/16 21:06
@@ -8,17 +10,15 @@ public class LockTest {
     public static void main(String[] args) {
         Redis redis = new Redis();
         redis.execute(jedis -> {
-            Long setnx = jedis.setnx("k1", "v1");
-            if(setnx==1){
-                //给锁设置过期时间，防止应用在运行过程中抛出异常导致锁无法正常释放 
+            String set = jedis.set("k1", "v1", new SetParams().nx().ex(5));
+            if(set!=null&& "OK".equals(set)){
                 jedis.expire("k1",5);
-                //正常情况下，返回 1, 表示不占位
-                jedis.set("money", String.valueOf(10000));
-                System.out.println(jedis.get("money"));
+                jedis.set("money", String.valueOf(100000));
+                System.out.println("money: "+jedis.get("money"));
                 // 释放资源
                 jedis.del("k1");
-
-            }else{
+            }
+            else{
                 //说明有其他线程占位置，需要停止操作
             }
         });
