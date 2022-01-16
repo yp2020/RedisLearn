4262004877576b414d135707a0bfa2d5db47684e
Wayne <3522373084@qq.com>
2022-01-16 21:25:18 +0800

    给锁设置过期时间，防止应用在运行过程中抛出异常导致锁无法正常释放

--- distributed_lock/src/main/java/com/study/distributed_lock/LockTest.java
+++ distributed_lock/src/main/java/com/study/distributed_lock/LockTest.java
@@ -10,6 +10,8 @@ public class LockTest {
         redis.execute(jedis -> {
             Long setnx = jedis.setnx("k1", "v1");
             if(setnx==1){
+                //给锁设置过期时间，防止应用在运行过程中抛出异常导致锁无法正常释放 
+                jedis.expire("k1",5);
                 //正常情况下，返回 1, 表示不占位
                 jedis.set("money", String.valueOf(10000));
                 System.out.println(jedis.get("money"));
